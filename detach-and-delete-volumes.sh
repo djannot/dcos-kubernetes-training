d0dddef26312251fa9610edca4d26d1f68fc6434
Administrator <admin@example.com>
2019-04-19 14:03:38 +0200

    Updating the detach-and-delete-volumes.sh script to remove the CSI volumes

--- detach-and-delete-volumes.sh
+++ detach-and-delete-volumes.sh
@@ -1,12 +1,17 @@
 eval $(maws login 110465657741_Mesosphere-PowerUser)
-name=djannot
-region=us-east-1
+name=$CLUSTER
+region=$REGION
 
-aws --region=$region ec2 describe-volumes |  jq --raw-output '.Volumes[] | select(.Tags[0].Value == "djannot") | .VolumeId' | while read volume; do
+aws --region=$region ec2 describe-volumes |  jq --raw-output ".Volumes[] | select(.Tags[0].Value == \"$name\") | .VolumeId" | while read volume; do
   instance=$(aws --region=$region ec2 describe-volumes --volume-ids $volume | jq --raw-output .Volumes[0].Attachments[0].InstanceId)
   aws --region=$region ec2 detach-volume --force --device=/dev/xvdb --instance-id=$instance --volume-id=$volume
 done
 sleep 15
-aws --region=$region ec2 describe-volumes |  jq --raw-output '.Volumes[] | select(.Tags[0].Value == "djannot") | .VolumeId' | while read volume; do
+aws --region=$region ec2 describe-volumes |  jq --raw-output ".Volumes[] | select(.Tags[0].Value == \"$name\") | .VolumeId" | while read volume; do
+  aws --region=$region ec2 delete-volume --volume-id=$volume
+done
+
+# Delete all the CSI volumes of this region that aren't in-use
+aws --region=$region ec2 describe-volumes |  jq --raw-output '.Volumes[] | select(.Tags[0].Key == "CSIVolumeName" and .State == "available") | .VolumeId' | while read volume; do
   aws --region=$region ec2 delete-volume --volume-id=$volume
 done
